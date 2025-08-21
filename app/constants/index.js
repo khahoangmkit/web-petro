8ce91d38cae00e398c856e7542cf44ce33ef468b
hoangdk <khahoangmkit@gmail.com>
2025-08-21 22:39:10 +0700

    update noi dung source

--- app/constants/index.js
+++ app/constants/index.js
@@ -47,7 +47,7 @@ export const dataHome = {
     ]
   },
   screen23: {
-    title: 'Công ty CP Nhiên liệu bay Petrolimex',
+    title: 'Petrolimex Aviation',
     background: '/backgrounds/screen_02.png',
     videoList: [
       '/dataSources/2_3_Nhien_lieu_bay_PA/hinh_anh_tra_nap_PA.mp4',
@@ -59,25 +59,21 @@ export const dataHome = {
     path: '#',
     children: [
       {
-        name: 'Hình ảnh tra nạp',
+        name: 'Tra nạp nguyên liệu',
         path: '/dataSources/2_3_Nhien_lieu_bay_PA/hinh_anh_tra_nap_PA.mp4',
       },
       {
-        name: 'Video 15 năm thành lập',
+        name: '15 năm thành lập',
         path: '/dataSources/2_3_Nhien_lieu_bay_PA/2.Video_15_nam_thanh_lap_PA.mp4',
       },
       {
-        name: 'Video về chuyến bay',
+        name: 'Tra nạp nhiên liệu sạch đầu tiên',
         path: '/dataSources/2_3_Nhien_lieu_bay_PA/3.Chuyen_tra_nap_dau_tien.mp4',
       },
       {
-        name: 'Video giới thiệu về công tác tra nạp',
+        name: 'giới thiệu công tác tra nạp',
         path: '/dataSources/2_3_Nhien_lieu_bay_PA/4.Quang_cao_TVC_QC.mp4',
       },
-      // {
-      //   name: 'Video giới thiệu về mẫu xe tra nạp',
-      //   path: '#',
-      // }
     ]
   },
 
@@ -86,42 +82,20 @@ export const dataHome = {
     title: 'Tổng Công Ty Vận Tải Thủy Petrolimex',
     background: '/backgrounds/screen_03.png',
     videoList: [
-      '/dataSources/4.TCT_van_tai_thuy/1.PGTANKER-CLip_hoi_nghi.mp4',
-      '/dataSources/4.TCT_van_tai_thuy/2.Video_tong_ket_hoi_nghi_khach_hang.mp4'
+      '/dataSources/4.TCT_van_tai_thuy/GIOI_THIEU_CHUNG_PG_TANKER.mp4',
+      '/dataSources/4.TCT_van_tai_thuy/PGTanker_nguoi_cam_co.mp4'
     ],
     name: 'PG Tanker',
     path: '#',
     children: [
       {
-        name: 'Ảnh các đội tàu',
-        path: '#'
+        name: 'Giới thiệu PG TANKER',
+        path: '/dataSources/4.TCT_van_tai_thuy/GIOI_THIEU_CHUNG_PG_TANKER.mp4'
       },
       {
-        name: 'Ảnh ban lãnh đạo',
-        path: '#'
-      },
-      {
-        name: 'Hội nghị đào tạo',
-        path: '/dataSources/4.TCT_van_tai_thuy/1.PGTANKER-CLip_hoi_nghi.mp4'
-      },
-      {
-        name: 'Clip của Tổng công ty',
-        path: '#',
-        children: [
-          {
-            name: 'Hội nghị khách hàng năm 2024',
-            path: '/dataSources/4.TCT_van_tai_thuy/2.Video_tong_ket_hoi_nghi_khach_hang.mp4'
-          },
-          {
-            name: 'Khóa đào tạo An toàn, an ninh mạng và Chuyển đổi số',
-            path: '#'
-          }
-        ]
-      },
-      {
-        name: 'Mô hình tàu',
-        path: '#'
-      },
+        name: 'PG TANKER - Người cắm cờ Petrolimex nơi biển xa',
+        path: '/dataSources/4.TCT_van_tai_thuy/PGTanker_nguoi_cam_co.mp4'
+      }
     ]
   },
 
@@ -143,39 +117,39 @@ export const dataHome = {
     path: '#',
     children: [
       {
-        name: 'Video giới thiệu về PIACOM',
+        name: 'giới thiệu về PIACOM',
         path: '/dataSources/5.Cty_cp_vien_thong/01.Gioi_thieu_PIACOM_TV.mp4'
       },
       {
-        name: 'Video PIACOM Introduction',
+        name: 'PIACOM Introduction',
         path: '/dataSources/5.Cty_cp_vien_thong/02.PIACOM introduction_Eng.mp4'
       },
       {
-        name: 'Video giới thiệu phần mềm EGAS',
+        name: 'giới thiệu phần mềm EGAS',
         path: '/dataSources/5.Cty_cp_vien_thong/03.PIA_Gioi_thieu_phan_mem_EGAS.mp4'
       },
       {
-        name: 'Video giới thiệu TĐH đo bồn bể',
+        name: 'giới thiệu TĐH đo bồn bể',
         path: '/dataSources/5.Cty_cp_vien_thong/04.PIA_Gioi_thieu_TDH_do_bon_be.mp4'
       },
       {
-        name: 'Video cơ chế TĐH đo bể',
+        name: 'cơ chế TĐH đo bể và thu nhận tín hiệu cột bơm CNXD',
         path: '/dataSources/5.Cty_cp_vien_thong/05.PIA_Co_che_do_be&thu_nhan_tin_hieu_cot_bom_CHXD.mp4'
       },
       {
-        name: 'Video PIA Code động',
+        name: 'PIA Code động',
         path: '/dataSources/5.Cty_cp_vien_thong/06.PIA_QR_code_dong+HDDT_quang_cao.mp4'
       },
       {
-        name: 'Video giới thiệu TĐH kho',
+        name: 'giới thiệu TĐH kho',
         path: '/dataSources/5.Cty_cp_vien_thong/08.PIA_GT_TDH_Kho_TAS.mp4'
       },
       {
-        name: 'Video giới thiệu Piacom erp',
+        name: 'giới thiệu Piacom erp',
         path: '/dataSources/5.Cty_cp_vien_thong/09.PIA_GT_PIACOM_ERP.mp4'
       },
       {
-        name: 'Video TĐH kho nhà bè case study',
+        name: 'TĐH kho nhà bè case study',
         path: '/dataSources/5.Cty_cp_vien_thong/10.PIA_TDH_Kho_Nha_be_Case_study.mp4'
       },
 
@@ -191,7 +165,11 @@ export const dataHome = {
       '/dataSources/67.Cty_CP_Petrolimex/Powersyn.mp4',
       '/dataSources/67.Cty_CP_Petrolimex/Scooter_30s_final.mp4',
       '/dataSources/67.Cty_CP_Petrolimex/PLC Cater CI-4 30s_final chuan.mp4',
-      '/dataSources/67.Cty_CP_Petrolimex/san_pham_Dau_mau_nhon.mp4'
+      '/dataSources/67.Cty_CP_Petrolimex/san_pham_Dau_mo_nhon.mp4',
+      '/dataSources/67.Cty_CP_Petrolimex/PETROLIMEX_NhuaDuong.mp4',
+      '/dataSources/67.Cty_CP_Petrolimex/15nam_nhuaduongfinal.mp4',
+      '/dataSources/67.Cty_CP_Petrolimex/nhua_duong_sp.mp4',
+      '/dataSources/67.Cty_CP_Petrolimex/hoa_chat_sp.mp4'
     ],
     name: 'Tổng Công ty Hóa dầu Petrolimex(PLC)',
     path: '#',
@@ -200,6 +178,14 @@ export const dataHome = {
         name: 'Dầu mỡ nhờn',
         path: '#',
         children: [
+          {
+            name: 'năng lực TCT PLC',
+            path: '/dataSources/67.Cty_CP_Petrolimex/PHIM_TCT_PETROLIMEX.mp4'
+          },
+          {
+            name: '30 năm thành lập PLC',
+            path: '/dataSources/67.Cty_CP_Petrolimex/30_nam_thanh_lap_Petrolimex.mp4'
+          },
           {
             name: 'Video quảng cáo sản phẩm',
             path: '#',
@@ -219,16 +205,8 @@ export const dataHome = {
             ]
           },
           {
-            name: 'Video năng lực TCT PLC',
-            path: '/dataSources/67.Cty_CP_Petrolimex/PHIM_TCT_PETROLIMEX.mp4'
-          },
-          {
-            name: 'Video 30 năm thành lập PLC',
-            path: '/dataSources/67.Cty_CP_Petrolimex/30_nam_thanh_lap_Petrolimex.mp4'
-          },
-          {
-            name: 'Danh mục sản phẩm Dầu mỡ nhờn',
-            path: '/dataSources/67.Cty_CP_Petrolimex/san_pham_Dau_mau_nhon.mp4'
+            name: 'sản phẩm Dầu mỡ nhờn',
+            path: '/dataSources/67.Cty_CP_Petrolimex/san_pham_Dau_mo_nhon.mp4'
           }
         ]
       },
@@ -237,28 +215,22 @@ export const dataHome = {
         path: '#',
         children: [
           {
-            name: 'Sản phẩm nhựa đường',
-            path: '#'
-          },
-          {
-            name: 'Video giới thiệu Công ty',
+            name: 'Giới thiệu Công ty',
             path: '/dataSources/67.Cty_CP_Petrolimex/PETROLIMEX_NhuaDuong.mp4'
           },
           {
-            name: 'Video 15 năm thành lập TCT',
+            name: '15 năm thành lập',
             path: '/dataSources/67.Cty_CP_Petrolimex/15nam_nhuaduongfinal.mp4'
+          },
+          {
+            name: 'Sản phẩm nhựa đường',
+            path: '/dataSources/67.Cty_CP_Petrolimex/nhua_duong_sp.mp4'
           }
         ]
       },
       {
         name: 'Hóa chất',
-        path: '#',
-        children: [
-          {
-            name: 'Sản phẩm hóa chất',
-            path: '/dataSources/67.Cty_CP_Petrolimex/hoa_chat.mp4'
-          }
-        ]
+        path: '/dataSources/67.Cty_CP_Petrolimex/hoa_chat_sp.mp4',
       }
     ]
   },
@@ -267,23 +239,18 @@ export const dataHome = {
     title: 'Tổng Công ty CP Gas Petrolimex (PGC - PGas)',
     background: '/backgrounds/screen_05.png',
     videoList: [
-      '/dataSources/1.Chuyen_doi_so/2.Ban Cong nghệ Thông tin/1.Quản lý đơn hàng-Cửa hàng yêu cầu tiếp tục giao hàng - Copy.mp4',
-      '/dataSources/1.Chuyen_doi_so/2.Ban Cong nghệ Thông tin/2.Định danh tài khoản bằng NFC.mp4',
-      '/dataSources/1.Chuyen_doi_so/2.Ban Cong nghệ Thông tin/3.Tài khoản liên kết.mp4'
+      '/dataSources/8.Tổng công ty Gas Petrolimex/Gas_mang_nguon_nang_luong_sach.mp4',
+      '/dataSources/8.Tổng công ty Gas Petrolimex/Hinh_anh_GAS.mp4',
     ],
     name: 'Tổng Công ty CP Gas Petrolimex (PGC - PGas)',
     path: '#',
     children: [
       {
-        name: 'Vận chuyển, đặc thù chung của Gas',
-        path: '#'
-      },
-      {
-        name: 'Gas mang nguồn năng lượng sạch',
+        name: 'Giới thiệu công ty',
         path: '/dataSources/8.Tổng công ty Gas Petrolimex/Gas_mang_nguon_nang_luong_sach.mp4'
       },
       {
-        name: 'Hình ảnh sản phẩm Gas',
+        name: 'Sản xuất, vận chuyển và sản phẩm Gas',
         path: '/dataSources/8.Tổng công ty Gas Petrolimex/Hinh_anh_GAS.mp4'
       }
     ]
@@ -293,28 +260,29 @@ export const dataHome = {
     title: 'Công ty CP Thiết bị Xăng dầu Petrolimex (PECO)',
     background: '/backgrounds/screen_06.png',
     videoList: [
-      '/dataSources/1.Chuyen_doi_so/2.Ban Cong nghệ Thông tin/1.Quản lý đơn hàng-Cửa hàng yêu cầu tiếp tục giao hàng - Copy.mp4',
-      '/dataSources/1.Chuyen_doi_so/2.Ban Cong nghệ Thông tin/2.Định danh tài khoản bằng NFC.mp4',
-      '/dataSources/1.Chuyen_doi_so/2.Ban Cong nghệ Thông tin/3.Tài khoản liên kết.mp4'
+      '/dataSources/9.ThietBiXangDauPECO/PECO-cac_hinh_anh_sx.mp4',
+      '/dataSources/9.ThietBiXangDauPECO/PECO-co_so_vat_chat.mp4',
+      '/dataSources/9.ThietBiXangDauPECO/PECO-hoat_dong_sx_kd.mp4',
+      '/dataSources/9.ThietBiXangDauPECO/PECO-hinh_anh_cot_bom.mp4'
     ],
     name: 'Công ty CP Thiết bị Xăng dầu Petrolimex (PECO)',
     path: '#',
     children: [
       {
-        name: 'Sản xuất niên PECO',
-        path: '#'
+        name: 'Hình ảnh sản xuất',
+        path: '/dataSources/9.ThietBiXangDauPECO/PECO-cac_hinh_anh_sx.mp4'
       },
       {
-        name: 'Sản xuất cột bơm tại nhà máy PECO',
-        path: '#'
+        name: 'Cơ sở vật chất',
+        path: '/dataSources/9.ThietBiXangDauPECO/PECO-co_so_vat_chat.mp4'
       },
       {
-        name: 'Phòng nghiên cứu sản phẩm mới',
-        path: '#'
+        name: 'Hoạt động sản xuất kinh doanh',
+        path: '/dataSources/9.ThietBiXangDauPECO/PECO-hoat_dong_sx_kd.mp4'
       },
       {
-        name: 'Linh kiện cột bơm Tatsuno',
-        path: '#'
+        name: 'Hình ảnh sản phẩm',
+        path: '/dataSources/9.ThietBiXangDauPECO/PECO-hinh_anh_cot_bom.mp4'
       }
     ]
   }
